8db6505f0727dcfbea852a2e14e68050ae7cc6a2
sumancfc <sumanstha999@gmail.com>
2020-10-28 16:01:44 +0545

    Validation added

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const {
+  userSigninValidator,
+  userSigninValidator,
+} = require("../validators/auth");
+const { runValidation } = require("../validators");
 
 router.get("/", (req, res) => {
   res.send("User ");
